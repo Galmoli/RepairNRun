600d4a7aee5403064a831af91017c51cae8354c5
MartiGuillot <MartiGuillot@users.noreply.github.com>
2020-01-31 20:46:27 +0100

    Base de la hand feta
    -Hand es mou amb el right stick
    -Hi ha dos tipus de problemes q ha d resoldre el player
    -El primer ha de posar la ma a sobrei clicar amb el stick
    -El segon ha de posar la ma a sobre i clicar A
    -Hi ha un spawner que els fa spawnejar aleatoriament cada 3 segons

---
+++ GlobalGameJam/Assets/Scripts/Hand.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    [HideInInspector] public hGamepad player1Hand;
+    public float handSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player1Hand = hinput.gamepad[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.position += player1Hand.rightStick.worldPositionCamera * handSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("firstProblem"))
+        {
+            //Aqui s'hauria d'activar un timer al problema que el faria desapareixer en x segons
+            Debug.Log("tocat");
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("firstProblem") && player1Hand.rightStickClick)
+        {
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("secondProblem") && player1Hand.A)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
