3f07f70b6aa93870f2c8e3ce23bd43f20529892f
MartiGuillot <MartiGuillot@users.noreply.github.com>
2020-02-01 13:04:25 +0100

    Particles i tal

--- GlobalGameJam/Assets/Scripts/ButtonMenu.cs
+++ GlobalGameJam/Assets/Scripts/ButtonMenu.cs
@@ -7,25 +7,26 @@ using UnityEngine.SceneManagement;
 public class ButtonMenu : MonoBehaviour
 {
     [HideInInspector] public bool canPressButton = false;
+    [HideInInspector] public Animator buttonAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        buttonAnimator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (canPressButton && hinput.anyGamepad.A)
+        if (canPressButton && (hinput.anyGamepad.A || hinput.anyGamepad.X || hinput.anyGamepad.B || hinput.anyGamepad.Y || hinput.anyGamepad.leftStickClick))
         {
-            SceneManager.LoadScene("MainScene");
+            buttonAnimator.SetTrigger("buttonClick");
+            //SceneManager.LoadScene("MainScene");
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("entra");
         if (other.CompareTag("Hand"))
         {
             canPressButton = true;
