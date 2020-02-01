414f33e20cd33c52238735c8d11d9a92eadcadb1
MartiGuillot <MartiGuillot@users.noreply.github.com>
2020-02-01 19:48:01 +0100

    Sonido de start arreglado, mano implementada
    Falta la puta animación que me está dejando jodido

--- GlobalGameJam/Assets/Scripts/Car.cs
+++ GlobalGameJam/Assets/Scripts/Car.cs
@@ -29,7 +29,7 @@ public class Car : MonoBehaviour
         if (isPlayer)
         {
             _carMovement = GetComponent<CarMovement>();
-            maxTorque = _carAIMove.maxMotorTorque;
+            maxTorque = _carMovement.maxMotorTorque;
         }
         else
         {
