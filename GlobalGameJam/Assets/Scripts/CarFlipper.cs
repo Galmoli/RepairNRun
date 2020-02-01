08677bb41df81150f7895c4dab24dbf73796dffc
Toni Galmés Oliver <tonigalmes98@gmail.com>
2020-02-01 15:26:40 +0100

    Car flipper

--- GlobalGameJam/Assets/Scripts/CarFlipper.cs
+++ GlobalGameJam/Assets/Scripts/CarFlipper.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarFlipper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float timeToFlip;
+    private float currentTime;
+
+    private void Update()
     {
-        
+        float angle = Vector3.Dot(transform.up, Vector3.up);
+        if (angle < 0.5f)
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime >= timeToFlip) FlipCar();
+        }
+        else currentTime = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FlipCar()
     {
-        
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.up), 0.1f);
     }
 }
