9f4b3e63461c104b6bb1b217a710a2aaf95e9ff5
MartiGuillot <MartiGuillot@users.noreply.github.com>
2020-02-01 14:18:41 +0100

    El menu funciona more or less, el codi es basura aixi q intentem no tocarne gaire

--- GlobalGameJam/Assets/Scripts/ButtonMenu.cs
+++ GlobalGameJam/Assets/Scripts/ButtonMenu.cs
@@ -8,11 +8,23 @@ public class ButtonMenu : MonoBehaviour
 {
     [HideInInspector] public bool canPressButton = false;
     [HideInInspector] public Animator buttonAnimator;
+    [HideInInspector] public bool triggerAnimationEnd = false;
+
+    public float animationEndTimer = 0.15f;
+    [HideInInspector] public float animationEndTimerCapsule = 0;
+
+    AudioSource buttonClick;
+    public ParticleSystem pSystem;
+
+    [HideInInspector] public bool lastSceneSender = false;
+    [HideInInspector] public float lastSceneTimer;
 
     // Start is called before the first frame update
     void Start()
     {
         buttonAnimator = GetComponentInChildren<Animator>();
+        buttonClick = this.GetComponent<AudioSource>();
+        lastSceneTimer = 0;
     }
 
     // Update is called once per frame
@@ -21,7 +33,26 @@ public class ButtonMenu : MonoBehaviour
         if (canPressButton && (hinput.anyGamepad.A || hinput.anyGamepad.X || hinput.anyGamepad.B || hinput.anyGamepad.Y || hinput.anyGamepad.leftStickClick))
         {
             buttonAnimator.SetTrigger("buttonClick");
+            triggerAnimationEnd = true;
             //SceneManager.LoadScene("MainScene");
+        }
+        if (triggerAnimationEnd)
+        {
+            animationEndTimerCapsule += Time.deltaTime;
+        }
+        if (animationEndTimerCapsule >= animationEndTimer)
+        {
+            if (!buttonClick.isPlaying && !lastSceneSender)
+            {
+                buttonClick.Play();
+                lastSceneSender = true;
+            }
+            if (!pSystem.isPlaying) pSystem.Play();
+            lastSceneTimer += Time.deltaTime;
+        }
+        if (lastSceneTimer >= 1)
+        {
+            SceneManager.LoadScene("MainScene");
         }
     }
 
